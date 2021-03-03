d14594e4c0e17b72b323814ecb0e83779270a4f4
Kyle <55870753+KyleLukassen@users.noreply.github.com>
2021-03-03 15:16:41 +0100

    Update

--- PHP/PHPJStest.php
+++ PHP/PHPJStest.php
@@ -1,10 +1,15 @@
 <?php
+        if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
+    {
+        testFile();
+    }
+    
 function testFile(){
     $TestFile = fopen("TestData.txt", "w") or die ("Unable to create file");
     $txt = "Cowabunga \n";
     fwrite($TestFile, $txt);
     fclose($TestFile);
-    };
+    }
 ?>
 <script type="text/javascript">
 var x = "Something"
