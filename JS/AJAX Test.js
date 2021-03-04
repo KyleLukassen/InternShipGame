26b3b145504a6c887d7ca3ad388e62175485bb2b
Kyle <55870753+KyleLukassen@users.noreply.github.com>
2021-03-04 14:51:02 +0100

    Text file opener

--- JS/AJAX Test.js
+++ JS/AJAX Test.js
@@ -1,10 +1,12 @@
-function loadDoc() {
+//function to load file
+function loadDoc(TextFile) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("demo").innerHTML = this.responseText;
+        //displays text of the document
+        document.getElementById("demo").innerHTML = this.responseText;
       }
     };
-    xhttp.open("GET", "JS/ajax_info.txt", true);
+    xhttp.open("GET", TextFile, true);
     xhttp.send();
   }
