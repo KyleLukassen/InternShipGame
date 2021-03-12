d52fdc54852ab49ad12fd799d9d912b0a3f91303
Kyle <55870753+KyleLukassen@users.noreply.github.com>
2021-03-12 13:02:13 +0100

    Update Battle.js

--- JS/Battle.js
+++ JS/Battle.js
@@ -1,4 +1,17 @@
+var IconArray = {Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8};
+var EnemyCharacterArray = {Enemy1, Enemy2, Enemy3, Enemy4};
+
+var EnemytestArray = {Bandit, Golem, Pirate, Witch};
+var Bandit = ["Bandit", "lvl2"];
+var Golem = ["Golem", "lvl3"];
+var Pirate = ["Pirate", "lvl6"];
+var Witch = ["Witch", "lvl8"];
+
 function BattleScreenFill(){
+    var EnemiesToFight = {};
+    while (EnemiesToFight.length < 4){
+        var SelectedEnemy = Math.random(1, EnemytestArray.length + 1);
+    }
     console.log("Somehow,.....it worked");
     document.getElementById("Icon1").innerHTML = "<img src='./Images/Placeholder.png'>"
 }
