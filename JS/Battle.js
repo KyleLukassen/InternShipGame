1390fdb31b296d1297ff3c936a13aa2608f18412
Kyle <55870753+KyleLukassen@users.noreply.github.com>
2021-03-12 16:59:35 +0100

    Update Battle.js

--- JS/Battle.js
+++ JS/Battle.js
@@ -1,17 +1,30 @@
-var IconArray = {Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8};
-var EnemyCharacterArray = {Enemy1, Enemy2, Enemy3, Enemy4};
+var IconArray = ["Icon1", "Icon2", "Icon3", "Icon4", "Icon5", "Icon6", "Icon7", "Icon8"];
+var EnemyCharacterArray = ["Enemy1", "Enemy2", "Enemy3", "Enemy4"];
 
-var EnemytestArray = {Bandit, Golem, Pirate, Witch};
-var Bandit = ["Bandit", "lvl2"];
-var Golem = ["Golem", "lvl3"];
-var Pirate = ["Pirate", "lvl6"];
-var Witch = ["Witch", "lvl8"];
+var Bandit = {TName: "Bandit", TLevel: 2};
+var Golem = {TName: "Golem", TLevel: 3};
+var Pirate = {TName: "Pirate", TLevel: 6};
+var Witch = {TName: "Witch", TLevel: 8};
+var EnemytestArray = [Bandit, Golem, Pirate, Witch];
+
+
+//function FindEnemy(Enemy){
+//    return Enemy.Level == 2;
+//}
 
 function BattleScreenFill(){
-    var EnemiesToFight = {};
+    var EnemiesToFight = [];
     while (EnemiesToFight.length < 4){
-        var SelectedEnemy = Math.random(1, EnemytestArray.length + 1);
+        var SelectedEnemy = Math.floor(Math.random() * (EnemytestArray.length - 1)); 
+        //var SelectedEnemy = Math.random(EnemytestArray.length + 1);
+        console.log(SelectedEnemy);
+        //console.log(EnemytestArray.find(FindEnemy));
+        console.log(EnemytestArray[0]);
+        console.log(EnemytestArray[1]);
+        console.log(EnemytestArray[Golem.TName]);
+        EnemiesToFight.push(EnemytestArray[SelectedEnemy]);
     }
-    console.log("Somehow,.....it worked");
+    console.log(EnemiesToFight.length);
+    console.log(EnemiesToFight);
     document.getElementById("Icon1").innerHTML = "<img src='./Images/Placeholder.png'>"
 }
