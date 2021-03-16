4f037d111cda19cb7265b520753e338f9cfebefa
Kyle <55870753+KyleLukassen@users.noreply.github.com>
2021-03-16 14:07:00 +0100

    update

--- JS/Battle.js
+++ JS/Battle.js
@@ -7,24 +7,31 @@ var Pirate = {TName: "Pirate", TLevel: 6};
 var Witch = {TName: "Witch", TLevel: 8};
 var EnemytestArray = [Bandit, Golem, Pirate, Witch];
 
-
-//function FindEnemy(Enemy){
-//    return Enemy.Level == 2;
-//}
+function FileArrayTest(TextFile){
+      //create new XMLHttpRequest
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var FileArray = this.responseText;
+        }
+      };
+      xhttp.open("GET", TextFile, true);
+      xhttp.send();
+      return FileArray;
+}
 
 function BattleScreenFill(){
     var EnemiesToFight = [];
     while (EnemiesToFight.length < 4){
         var SelectedEnemy = Math.floor(Math.random() * (EnemytestArray.length - 1)); 
-        //var SelectedEnemy = Math.random(EnemytestArray.length + 1);
         console.log(SelectedEnemy);
-        //console.log(EnemytestArray.find(FindEnemy));
-        console.log(EnemytestArray[0]);
-        console.log(EnemytestArray[1]);
         console.log(EnemytestArray[Golem.TName]);
         EnemiesToFight.push(EnemytestArray[SelectedEnemy]);
     }
     console.log(EnemiesToFight.length);
     console.log(EnemiesToFight);
-    document.getElementById("Icon1").innerHTML = "<img src='./Images/Placeholder.png'>"
+    document.getElementById("Icon1").innerHTML = "<img src='./Images/Placeholder.png'>";
+    FileArrayTest("./DataFiles/EnemyData.txt");
+    console.log(FileArray);
 }
+
